ed6285756ec7ff1c6fccca3d866ee1bf1dde4197
Todd Sharpe <toddsharpe@outlook.com>
2023-06-06 09:29:35 -0700

    Initial platform checkin, initial apps, integrate with UBuild

---
+++ Boards/artyz7/platform/build.sh
@@ -0,0 +1,9 @@
+#! /bin/bash
+
+# Clean
+#petalinux-build -x mrproper -f
+
+# Pull XSA again?
+
+petalinux-build
+petalinux-package --boot --fsbl ./images/linux/zynq_fsbl.elf --fpga ./images/linux/system.bit --u-boot --force
---
+++ Src/Hosted/App.h
@@ -0,0 +1,24 @@
+#include "Callback.h"
+
+#include <vector>
+#include <chrono>
+
+namespace Nighthawk
+{
+    class App
+    {
+    public:
+        void Install(const Callback& callback, const std::chrono::milliseconds period);
+        void Run();
+
+    private:
+        struct Dispatch
+        {
+            Callback Call;
+            std::chrono::milliseconds Period;
+            std::chrono::steady_clock::time_point Last;
+        };
+
+        std::vector<Dispatch> m_dispatches;
+    };
+}

1df16530554c4d4d29888cfd18085ae7ee45c651
Todd Sharpe <toddsharpe@outlook.com>
2023-10-10 22:30:49 -0700

    PMod OLED RGB initial commit

--- Boards/artyz7/platform/build.sh
+++ Boards/artyz7/platform/build.sh
@@ -4,6 +4,7 @@
 petalinux-build -x mrproper -f
 
 # Pull XSA again?
+#petalinux-config --get-hw-description ../hardware
 
 petalinux-build
 petalinux-package --boot --fsbl ./images/linux/zynq_fsbl.elf --fpga ./images/linux/system.bit --u-boot --force
